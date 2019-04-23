be85476fedf45b36dbef651467866f2e1199f98c
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2019-04-22 21:43:09 -0300

    Se agrego la estructura del Test del modulo Guardarropa, junto con 2 tests iniciales

---
+++ QueMePongo/src/test/java/dds/utn/ju_ma/group7/QueMePongo/GuardarropaTest.java
@@ -0,0 +1,83 @@
+package dds.utn.ju_ma.group7.QueMePongo;
+
+import static org.junit.Assert.*;
+
+import dds.utn.ju_ma.group7.QueMePongo.Prenda.Color;
+import dds.utn.ju_ma.group7.QueMePongo.Prenda.Prenda;
+import dds.utn.ju_ma.group7.QueMePongo.Prenda.PrendaBuilder;
+import dds.utn.ju_ma.group7.QueMePongo.Prenda.TipoPrenda;
+import dds.utn.ju_ma.group7.QueMePongo.Prenda.TipoTela;
+import dds.utn.ju_ma.group7.QueMePongo.Usuario.Guardarropa;
+
+import org.junit.Assert;
+import org.junit.Test; // for @Test 
+import org.junit.Before; // for @Before
+
+public class GuardarropaTest {
+	
+	private Guardarropa guardarropaCompleto = new Guardarropa();
+	private Guardarropa guardarropaIncompleto = new Guardarropa();
+	private PrendaBuilder remeraNegraBuilder = new PrendaBuilder();
+	private Prenda remeraNegra;
+	private PrendaBuilder jeanBuilder = new PrendaBuilder();
+	private Prenda jeanNegro;
+	private PrendaBuilder zapatillasBuilder = new PrendaBuilder();
+	private Prenda zapatillasNegras;
+	private PrendaBuilder collarDivinoBuilder = new PrendaBuilder();
+	private Prenda collarDivino;
+	private PrendaBuilder remeraBlancaBuilder = new PrendaBuilder();
+	private Prenda remeraBlanca;
+	private Color negro = new Color(0, 0, 0);
+	private Color blanco = new Color(255, 255, 255);
+	
+	@Before
+	public void init() {
+		
+		remeraNegraBuilder.setTipoPrenda(TipoPrenda.REMERA);
+		remeraNegraBuilder.setTipoTela(TipoTela.ALGODON);
+		remeraNegraBuilder.setColorPrimario(negro);
+		remeraNegra = remeraNegraBuilder.crearPrenda();
+		
+		remeraBlancaBuilder.setTipoPrenda(TipoPrenda.REMERA);
+		remeraBlancaBuilder.setTipoTela(TipoTela.ALGODON);
+		remeraBlancaBuilder.setColorPrimario(blanco);
+		remeraBlanca = remeraBlancaBuilder.crearPrenda();
+		
+		jeanBuilder.setTipoPrenda(TipoPrenda.JEAN);
+		jeanBuilder.setTipoTela(TipoTela.CUERO);
+		jeanBuilder.setColorPrimario(negro);
+		jeanNegro = jeanBuilder.crearPrenda();
+		
+		zapatillasBuilder.setTipoPrenda(TipoPrenda.ZAPATILLAS);
+		zapatillasBuilder.setTipoTela(TipoTela.NYLON);
+		zapatillasBuilder.setColorPrimario(negro);
+		zapatillasNegras = zapatillasBuilder.crearPrenda();
+		
+		collarDivinoBuilder.setTipoPrenda(TipoPrenda.COLLAR);
+		collarDivinoBuilder.setTipoTela(TipoTela.SEDA);
+		collarDivinoBuilder.setColorPrimario(blanco);
+		collarDivino = collarDivinoBuilder.crearPrenda();
+		
+		guardarropaCompleto.agregarPrenda(remeraNegra);
+		guardarropaCompleto.agregarPrenda(remeraBlanca);
+		guardarropaCompleto.agregarPrenda(jeanNegro);
+		guardarropaCompleto.agregarPrenda(zapatillasNegras);
+		guardarropaCompleto.agregarPrenda(collarDivino);
+		
+		guardarropaIncompleto.agregarPrenda(remeraNegra);
+		guardarropaIncompleto.agregarPrenda(jeanNegro);
+		guardarropaIncompleto.agregarPrenda(collarDivino);
+		
+	}
+	
+	@Test
+	public void elGuardarropaCompletoGeneraDosAtuendos() {
+		Assert.assertTrue(guardarropaCompleto.generarAtuendos().size() == 2);
+	}
+	
+	@Test
+	public void elGuardarropaIncompletoNoGeneraAtuendos() {
+		Assert.assertTrue(guardarropaIncompleto.generarAtuendos().size() == 0);
+	}
+
+}
