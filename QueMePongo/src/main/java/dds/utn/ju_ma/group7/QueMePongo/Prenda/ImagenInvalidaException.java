5ec0c7fdb05b7bb291cf803c0b0b9b3a677925a1
alejorodrig <alejo.meson@gmail.com>
2019-05-24 13:59:24 -0300

    La prenda puede incluir una imagen

---
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Prenda/ImagenInvalidaException.java
@@ -0,0 +1,10 @@
+package dds.utn.ju_ma.group7.QueMePongo.Prenda;
+
+public class ImagenInvalidaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ImagenInvalidaException(String mensaje) {
+		super(mensaje);
+	}
+}
--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Prenda/Prenda.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Prenda/Prenda.java
@@ -1,10 +1,17 @@
 package dds.utn.ju_ma.group7.QueMePongo.Prenda;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Prenda {
 	private final TipoPrenda tipoPrenda;
 	private final TipoTela tipoTela;
 	private final Color colorPrimario;
 	private final Color colorSecundario;
+	private BufferedImage imagen;
 
 	public Prenda(TipoPrenda tipoPrenda, TipoTela tipoTela, Color colorPrimario, Color colorSecundario) {
 		this.tipoPrenda = tipoPrenda;
@@ -31,6 +38,18 @@ public class Prenda {
 
 	public CategoriaPrenda getCategoria() {
 		return this.tipoPrenda.getCategoria();
+	}
+	
+	public BufferedImage getImagen() {
+		return this.imagen;
+	}
+	
+	public void setImagen(String path) {
+		try {
+			this.imagen = ImageIO.read(new File(path));
+		} catch (IOException e) {
+			throw new ImagenInvalidaException("No se pudo cargar la imagen");
+		}
 	}
 	
 	public boolean esDeCategoria(CategoriaPrenda categoria) {
