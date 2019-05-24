50f82f892520ef0aecd8da8c217f4e0d46c403ab
alejorodrig <alejo.meson@gmail.com>
2019-05-24 16:49:08 -0300

    Se agrega la clase Sugerencia junto con la enumeración de su estado

---
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Evento/EstadoSugerencia.java
@@ -0,0 +1,8 @@
+package dds.utn.ju_ma.group7.QueMePongo.Evento;
+
+public enum EstadoSugerencia {
+	PENDIENTE,
+	ACEPTADA,
+	RECHAZADA
+
+}
---
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Evento/Sugerencia.java
@@ -0,0 +1,34 @@
+package dds.utn.ju_ma.group7.QueMePongo.Evento;
+
+import dds.utn.ju_ma.group7.QueMePongo.Atuendo.Atuendo;
+
+public class Sugerencia {
+	private final Atuendo atuendo;
+	private EstadoSugerencia estado;
+	
+	public Sugerencia(Atuendo atuendo) {
+		this.atuendo = atuendo;
+		this.estado = EstadoSugerencia.PENDIENTE;
+	}
+	
+	public Atuendo getAtuendo() {
+		return atuendo;
+	}
+	
+	public EstadoSugerencia getEstado() {
+		return estado;
+	}
+	
+	private void setEstado(EstadoSugerencia estado) {
+		this.estado = estado;
+	}
+	
+	public void aceptar() {
+		this.setEstado(EstadoSugerencia.ACEPTADA);
+	}
+	
+	public void rechazar() {
+		this.setEstado(EstadoSugerencia.RECHAZADA);
+	}
+	
+}
