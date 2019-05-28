06a35145fe1e128b5dc45ffc6765110526d87636
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-05-28 14:47:10 -0300

    Se agregan mas tests en el sugeridor

--- QueMePongo/src/test/java/dds/utn/ju_ma/group7/QueMePongo/SugeridorTest.java
+++ QueMePongo/src/test/java/dds/utn/ju_ma/group7/QueMePongo/SugeridorTest.java
@@ -15,23 +15,17 @@ import dds.utn.ju_ma.group7.QueMePongo.Prenda.*;
 
 public class SugeridorTest extends Fixture {
 	
+	private PrendaBuilder buzoBuilder = new PrendaBuilder();
+	private Prenda buzo;
+	
 	private PrendaBuilder remeraNegraBuilder = new PrendaBuilder();
 	private Prenda remeraNegra;
-
-	private PrendaBuilder remeraBlancaBuilder = new PrendaBuilder();
-	private Prenda remeraBlanca;
 
 	private PrendaBuilder pantalonNegroBuilder = new PrendaBuilder();
 	private Prenda pantalonNegro;
 
-	private PrendaBuilder pantalonBlancoBuilder = new PrendaBuilder();
-	private Prenda pantalonBlanco;
-
 	private PrendaBuilder zapatosNegrosBuilder = new PrendaBuilder();
 	private Prenda zapatosNegros;
-
-	private PrendaBuilder zapatosBlancosBuilder = new PrendaBuilder();
-	private Prenda zapatosBlancos;
 
 	private PrendaBuilder collarBuilder = new PrendaBuilder();
 	private Prenda collar;
@@ -40,56 +34,64 @@ public class SugeridorTest extends Fixture {
 	
 	private ProveedorMockVerano proveedor1 = new ProveedorMockVerano();
 	private ProveedorMockInvierno proveedor2 = new ProveedorMockInvierno();
-	private Sugeridor sug = new Sugeridor();
+	private Sugeridor sug1 = new Sugeridor();
+	private Sugeridor sug2 = new Sugeridor();
 	
 	private Calendar fecha1 = Calendar.getInstance();
 	
-	List<Prenda> prendasSup = new ArrayList<Prenda>();
-	List<Atuendo> atuendos = new ArrayList<Atuendo>();
-	Atuendo atuendoNegro = new Atuendo(prendasSup,pantalonNegro,zapatosNegros,collar);
+	List<Prenda> prendasSupPobre = new ArrayList<Prenda>();
+	List<Atuendo> atuendosSoloVerano = new ArrayList<Atuendo>();
+	Atuendo atuendoNegro = new Atuendo(prendasSupPobre,pantalonNegro,zapatosNegros,collar);
+	
+	List<Prenda> prendasSupFuerte = new ArrayList<Prenda>();
+	List<Atuendo> atuendosVeranoEinvierno = new ArrayList<Atuendo>();
+	Atuendo atuendoNegroConBuzo = new Atuendo(prendasSupFuerte,pantalonNegro,zapatosNegros,collar);
 	
 	public void init() {
+		
+		buzoBuilder.setTipoPrenda(TipoPrenda.BUZO).setTipoTela(TipoTela.ALGODON).setColorPrimario(negro);
+		buzo = buzoBuilder.crearPrenda();
 		
 		remeraNegraBuilder.setTipoPrenda(TipoPrenda.REMERA).setTipoTela(TipoTela.ALGODON).setColorPrimario(negro);
 		remeraNegra = remeraNegraBuilder.crearPrenda();
 
-		remeraBlancaBuilder.setTipoPrenda(TipoPrenda.REMERA).setTipoTela(TipoTela.ALGODON).setColorPrimario(blanco);
-		remeraBlanca = remeraBlancaBuilder.crearPrenda();
-
 		pantalonNegroBuilder.setTipoPrenda(TipoPrenda.JOGGIN).setTipoTela(TipoTela.NYLON).setColorPrimario(negro);
 		pantalonNegro = pantalonNegroBuilder.crearPrenda();
-
-		pantalonBlancoBuilder.setTipoPrenda(TipoPrenda.JOGGIN).setTipoTela(TipoTela.NYLON).setColorPrimario(blanco);
-		pantalonBlanco = pantalonBlancoBuilder.crearPrenda();
 
 		zapatosNegrosBuilder.setTipoPrenda(TipoPrenda.ZAPATILLAS).setTipoTela(TipoTela.NYLON).setColorPrimario(negro);
 		zapatosNegros = zapatosNegrosBuilder.crearPrenda();
 
-		zapatosBlancosBuilder.setTipoPrenda(TipoPrenda.ZAPATILLAS).setTipoTela(TipoTela.NYLON).setColorPrimario(blanco);
-		zapatosBlancos = zapatosBlancosBuilder.crearPrenda();
-
 		collarBuilder.setTipoPrenda(TipoPrenda.COLLAR).setTipoTela(TipoTela.SEDA).setColorPrimario(negro);
 		collar = collarBuilder.crearPrenda();
 		
-		prendasSup.add(remeraNegra);
+		prendasSupPobre.add(remeraNegra);
+		prendasSupFuerte.add(remeraNegra);
+		prendasSupFuerte.add(buzo);
 		
 		
-		atuendos.add(atuendoNegro);
+		atuendosSoloVerano.add(atuendoNegro);
+		atuendosVeranoEinvierno.add(atuendoNegro);
+		atuendosVeranoEinvierno.add(atuendoNegroConBuzo);
 		
-		sug.setProveedorClima(proveedor2);
+		sug1.setProveedorClima(proveedor1);
+		sug2.setProveedorClima(proveedor2);
 		fecha1.set(2019, 05, 27, 18, 0);
 	}
 
 
 	@Test
-	public void sugiere() {
-		Assert.assertEquals(sug.sugerir(atuendos, fecha1).size(), 1);
+	public void sugiereRopaDeVeranoConMockVerano() { // al sugeridor le ofrezco solo el atuendo de verano y me lo devuelve
+		Assert.assertEquals(sug1.sugerir(atuendosSoloVerano, fecha1).size(), 1);
 	}
 	
+	@Test
+	public void sugiereRopaDeInviernoConMockInvierno() { // al sugeridor le ofrezco un atuendo de verano y otro de invierno y solo devuelve el de invierno
+		Assert.assertEquals(sug1.sugerir(atuendosVeranoEinvierno, fecha1).size(), 1);
+	}
 
 	@Test(expected = NullPointerException.class)
-	public void asd(){
-		sug.sugerir(atuendos, fecha1);
+	public void pruebaDeNull(){
+		sug1.sugerir(atuendosVeranoEinvierno, fecha1);
 	}
 
 }
