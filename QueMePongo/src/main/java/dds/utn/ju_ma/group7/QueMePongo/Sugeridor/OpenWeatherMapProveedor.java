a6c68d787e7aa0700e365f7fb5cb3f69c4e00519
matiasmarmo <matumarmo@gmail.com>
2019-05-29 10:01:57 -0300

    Se modifico el metodo redondearFecha para fechas criticas

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/OpenWeatherMapProveedor.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/OpenWeatherMapProveedor.java
@@ -91,7 +91,10 @@ public class OpenWeatherMapProveedor implements ProveedorClima {
 	
 	private Calendar redondearFecha(Calendar fecha) {
 		int hour = fecha.get(Calendar.HOUR_OF_DAY);
-		if(hour % 3 == 1) { fecha.set(Calendar.HOUR_OF_DAY, hour+2); }
+		int day = fecha.get(Calendar.DAY_OF_MONTH);
+		if(hour == 24) { fecha.set(Calendar.HOUR_OF_DAY, 0); fecha.set(Calendar.DAY_OF_MONTH, day + 1); }
+		else if(hour == 23 || hour == 22) { fecha.set(Calendar.HOUR_OF_DAY, 21); }
+		else if(hour % 3 == 1) { fecha.set(Calendar.HOUR_OF_DAY, hour+2); }
 		else if(hour % 3 == 2) { fecha.set(Calendar.HOUR_OF_DAY, hour+1); }
 		return fecha;
 	}
