d65ae374670d9f0874c5b273e017d18cd64dd546
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-05-28 23:00:57 -0300

    Actualizar JobEventos

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Evento/JobEventos.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Evento/JobEventos.java
@@ -2,14 +2,31 @@ package dds.utn.ju_ma.group7.QueMePongo.Evento;
 
 import java.util.Calendar;
 import java.util.List;
+import java.util.Timer;
 import java.util.TimerTask;
 
-public class JobEventos extends TimerTask {
+import dds.utn.ju_ma.group7.QueMePongo.Sugeridor.ProveedorClima;
+import dds.utn.ju_ma.group7.QueMePongo.Sugeridor.Sugeridor;
 
+public class JobEventos extends TimerTask {
+	
+	public static Timer instanciarJobEventos(ProveedorClima proveedorClima, long periodo) {
+		JobEventos job = new JobEventos(proveedorClima);
+		Timer timer = new Timer(false);
+		timer.scheduleAtFixedRate(job, 0, periodo);
+		return timer;
+	}
+	
+	private Sugeridor sugeridor;
+	
+	private JobEventos(ProveedorClima proveedorClima) {
+		this.sugeridor = new Sugeridor(proveedorClima);
+	}
+	
 	@Override
 	public void run() {
 		List<Evento> eventosProximos = RepositorioEventos.eventosProximos(Calendar.getInstance());
-		eventosProximos.stream().forEach(unEvento -> unEvento.serSugerido(Calendar.getInstance()));
+		eventosProximos.forEach(unEvento -> this.sugeridor.sugerir(unEvento));
 	}
 
 }
