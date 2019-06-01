964fd6bdc71d5df2067578df6abb8fc4986a3b2e
NicolasDominguez98 <nicodominguez@est.frba.utn.edu.ar>
2019-06-01 17:28:41 -0300

    HttpProveedor contiene la logica de conexion a las APIs, los adaptadores heredan esta logica

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/AccuWeatherProveedor.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/AccuWeatherProveedor.java
@@ -1,54 +1,28 @@
 package dds.utn.ju_ma.group7.QueMePongo.Sugeridor;
 
-import java.io.StringReader;
 import java.util.Calendar;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.ws.rs.core.MediaType;
+import com.sun.jersey.api.client.WebResource;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
+public class AccuWeatherProveedor extends HttpProveedor {
 
-import dds.utn.ju_ma.group7.QueMePongo.Excepciones.FechaInexistenteException;
-
-public class AccuWeatherProveedor implements ProveedorClima {
-	
-	private Client client;
-	private static final String API = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/7894";
-	private String key_id = "0yMLbaqAcUXajQDilhGxZNGZnhWDl1SP";
-	
-	public AccuWeatherProveedor() {
-		this.client = Client.create();
+	protected String api() {
+		return "http://dataservice.accuweather.com/forecasts/v1/daily/5day/7894";
 	}
 	
-	private JsonArray getPronosticoCincoDias() {
-		ClientResponse response = 
-				this.client.resource(API)
-				.queryParam("apikey", key_id)
+	protected String key_id() {
+		return "0yMLbaqAcUXajQDilhGxZNGZnhWDl1SP";
+	}
+	
+	protected WebResource parametrosRequest(WebResource resource) {
+		return resource
+				.queryParam("apikey", key_id())
 				.queryParam("language", "es-ar")
 				.queryParam("details", "true")
-				.queryParam("metrics", "true")
-				.accept(MediaType.APPLICATION_JSON)
-				.get(ClientResponse.class);
-
-		String jsonString = response.getEntity(String.class);
-		response.close();
-		client.destroy();
-		
-		JsonReader pronosticoJsonReader = Json.createReader(new StringReader(jsonString));
-		JsonObject pronostico = pronosticoJsonReader.readObject();
-		pronosticoJsonReader.close();
-		JsonArray pronosticoCincoDias = pronostico.getJsonArray("DailyForecasts");
-		return pronosticoCincoDias;
+				.queryParam("metrics", "true");
 	}
 	
-	private Calendar strignJsonToCalendar(String fecha) {
+	protected Calendar strignJsonToCalendar(String fecha) {
 //		formato de fecha en json: "2019-05-27T07:00:00+07:00"
 // 		No nos interesan las horas ya que AccuWeather no tiene precision con respecto a las horas
 		String[] camposFecha = fecha.split("T");
@@ -61,43 +35,12 @@ public class AccuWeatherProveedor implements ProveedorClima {
 		return fechaCalendar;
 	}
 	
-	private boolean fechaCoincide(String fechaString, Calendar fechaBuscada) {
-		Calendar fechaCalendar = strignJsonToCalendar(fechaString);
-		int diaProvisto = fechaCalendar.get(Calendar.DAY_OF_MONTH);
-		int diaBuscado = fechaBuscada.get(Calendar.DAY_OF_MONTH);
-		boolean coinciden = diaBuscado == diaProvisto;
-		return coinciden;
+	protected String campoFecha() {
+		return "Date";
 	}
 	
-	private Stream<JsonObject> jsonArrayToStream(JsonArray jsonArray){
-		return jsonArray.stream().map(JsonValue -> JsonValue.asJsonObject());
-	}
-	
-	private JsonObject filtrarPorFecha(Stream<JsonObject> pronosticos, Calendar fecha) {
-		List<JsonObject> pronosticoBuscadoLista = pronosticos
-				.filter(pronostico -> fechaCoincide(pronostico.getString("Date"), fecha))
-				.collect(Collectors.toList());
-		
-		if(pronosticoBuscadoLista.size() == 0) {
-			throw new FechaInexistenteException("No existe la fecha buscada dentro del pronostico de 5 dias");			
-		} else {
-			System.out.println(pronosticoBuscadoLista.size());
-			return pronosticoBuscadoLista.iterator().next();
-		}
-	}
-	
-	private JsonObject pronosticoEspecifico(Calendar fechaBuscada) {
-		JsonArray pronosticoCincoDias = getPronosticoCincoDias();
-		Stream<JsonObject> pronosticosStream = jsonArrayToStream(pronosticoCincoDias);
-		return filtrarPorFecha(pronosticosStream, fechaBuscada);
-	}
-	
-	private double fahrenheitToCelsius(double gradosFahrenheit) {
-		return (gradosFahrenheit - 32)* 5/9;
-	}
-	
-	private double truncarADosDecimales(double numero) {
-	    return Math.floor(numero * 100) / 100;
+	protected String campoListaPronosticos() {
+		return "DailyForecasts";
 	}
 	
 	public double getTemperatura(Calendar fecha) {
@@ -109,6 +52,10 @@ public class AccuWeatherProveedor implements ProveedorClima {
 		double maximaC = fahrenheitToCelsius(maximaF);
 		double resC = (minimaC + maximaC) / 2;
 		return truncarADosDecimales(resC);
+	}
+	
+	private double fahrenheitToCelsius(double gradosFahrenheit) {
+		return (gradosFahrenheit - 32)* 5/9;
 	}
 
 }
--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/OpenWeatherMapProveedor.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/OpenWeatherMapProveedor.java
@@ -1,56 +1,30 @@
 package dds.utn.ju_ma.group7.QueMePongo.Sugeridor;
 
-import java.io.StringReader;
 import java.util.Calendar;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import javax.json.Json;
-import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
-import javax.ws.rs.core.MediaType;
+import com.sun.jersey.api.client.WebResource;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
+public class OpenWeatherMapProveedor extends HttpProveedor {
 
-import dds.utn.ju_ma.group7.QueMePongo.Excepciones.FechaInexistenteException;
-
-public class OpenWeatherMapProveedor implements ProveedorClima {
+	private String lon_CABA = "-58.450001";
+	private String lat_CABA = "-34.599998";
 	
-	private Client client;
-	private static final String API = "http://api.openweathermap.org/data/2.5/forecast/";
-	public String lon_CABA = "-58.450001";
-	public String lat_CABA = "-34.599998";
-	private String key_id = "a40660bcb12fb0790f9455769b86f8ed";
-
-	public OpenWeatherMapProveedor() {
-		this.client = Client.create();
+	protected String api() {
+		return "http://api.openweathermap.org/data/2.5/forecast/";
 	}
-
-	private JsonArray getPronosticoCincoDias() {
-		ClientResponse response = 
-				this.client.resource(API)
-				.queryParam("lat", lat_CABA)
+	
+	protected String key_id() {
+		return "a40660bcb12fb0790f9455769b86f8ed";
+	}
+	
+	protected WebResource parametrosRequest(WebResource resource) {
+		return resource
 				.queryParam("lon", lon_CABA)
-				.queryParam("appid", key_id)
-				.accept(MediaType.APPLICATION_JSON)
-				.get(ClientResponse.class);
-
-		String jsonString = response.getEntity(String.class);
-		response.close();
-		client.destroy();
-		
-		JsonReader pronosticoJsonReader = Json.createReader(new StringReader(jsonString));
-		JsonObject pronostico = pronosticoJsonReader.readObject();
-		pronosticoJsonReader.close();
-		JsonArray pronosticoCincoDias = pronostico.getJsonArray("list");
-		return pronosticoCincoDias;
+				.queryParam("lat", lat_CABA)
+				.queryParam("appid", key_id());
 	}
 
-	private Calendar strignJsonToCalendar(String fecha) {
+	protected Calendar strignJsonToCalendar(String fecha) {
 //		formato de fecha en json: "2019-05-25 18:00:00"
 		String[] camposFecha = fecha.split(" ");
 		String[] camposDiaCompleto = camposFecha[0].split("-");
@@ -64,43 +38,15 @@ public class OpenWeatherMapProveedor implements ProveedorClima {
 				Integer.parseInt(campoHora), 00);
 		return fechaCalendar;
 	}
-
-	private boolean fechaCoincide(String fechaString, Calendar fechaBuscada) {
-		Calendar fechaCalendar = strignJsonToCalendar(fechaString);
-		return fechaCalendar.get(Calendar.DAY_OF_MONTH) == fechaBuscada.get(Calendar.DAY_OF_MONTH)
-				&& fechaCalendar.get(Calendar.HOUR_OF_DAY) == fechaBuscada.get(Calendar.HOUR_OF_DAY);
+	
+	protected String campoFecha() {
+		return "dt_txt";
 	}
 	
-	private Stream<JsonObject> jsonArraytoStream(JsonArray jsonArray){
-		return jsonArray.stream().map(JsonValue -> JsonValue.asJsonObject());
+	protected String campoListaPronosticos() {
+		return "list";
 	}
 	
-	private JsonObject filtrarPorFecha(Stream<JsonObject> pronosticos, Calendar fecha) {
-		List<JsonObject> pronosticoBuscadoLista = pronosticos
-				.filter(pronostico -> fechaCoincide(pronostico.getString("dt_txt"), fecha))
-				.collect(Collectors.toList());
-		
-		if(pronosticoBuscadoLista.size() == 0) {
-			throw new FechaInexistenteException("No existe la fecha buscada dentro del pronostico de 5 dias");			
-		} else {
-			return pronosticoBuscadoLista.iterator().next();
-		}
-	}
-
-	private JsonObject pronosticoEspecifico(Calendar fechaBuscada) {
-		JsonArray pronosticoCincoDias = getPronosticoCincoDias();
-		Stream<JsonObject> pronosticosStream = jsonArraytoStream(pronosticoCincoDias);
-		return filtrarPorFecha(pronosticosStream, fechaBuscada);
-	}
-
-	private double kelvinToCelsius(double gradosKelvin) {
-		return gradosKelvin - 273.15;
-	}
-	
-	private double truncarADosDecimales(double numero) {
-	    return Math.floor(numero * 100) / 100;
-	}
-
 	public double getTemperatura(Calendar fecha) {
 		fecha = redondearFecha(fecha);
 		JsonObject pronostico = pronosticoEspecifico(fecha);
@@ -108,14 +54,30 @@ public class OpenWeatherMapProveedor implements ProveedorClima {
 		double temperaturaC = kelvinToCelsius(temperaturaK);
 		return truncarADosDecimales(temperaturaC);
 	}
-	
+
+	protected boolean fechaCoincide(String fechaString, Calendar fechaBuscada) {
+		Calendar fechaCalendar = strignJsonToCalendar(fechaString);
+		return super.fechaCoincide(fechaString, fechaBuscada)
+				&& fechaCalendar.get(Calendar.HOUR_OF_DAY) == fechaBuscada.get(Calendar.HOUR_OF_DAY);
+	}
+
+	private double kelvinToCelsius(double gradosKelvin) {
+		return gradosKelvin - 273.15;
+	}
+
 	private Calendar redondearFecha(Calendar fecha) {
 		int hour = fecha.get(Calendar.HOUR_OF_DAY);
 		int day = fecha.get(Calendar.DAY_OF_MONTH);
-		if(hour == 24) { fecha.set(Calendar.HOUR_OF_DAY, 0); fecha.set(Calendar.DAY_OF_MONTH, day + 1); }
-		else if(hour == 23 || hour == 22) { fecha.set(Calendar.HOUR_OF_DAY, 21); }
-		else if(hour % 3 == 1) { fecha.set(Calendar.HOUR_OF_DAY, hour+2); }
-		else if(hour % 3 == 2) { fecha.set(Calendar.HOUR_OF_DAY, hour+1); }
+		if (hour == 24) {
+			fecha.set(Calendar.HOUR_OF_DAY, 0);
+			fecha.set(Calendar.DAY_OF_MONTH, day + 1);
+		} else if (hour == 23 || hour == 22) {
+			fecha.set(Calendar.HOUR_OF_DAY, 21);
+		} else if (hour % 3 == 1) {
+			fecha.set(Calendar.HOUR_OF_DAY, hour + 2);
+		} else if (hour % 3 == 2) {
+			fecha.set(Calendar.HOUR_OF_DAY, hour + 1);
+		}
 		return fecha;
 	}
 }
