ffd7edf745f35beb18ff9983603303631cb7a5ba
matiasmarmo <matumarmo@gmail.com>
2019-06-17 19:37:35 -0300

    Se modifico el redondear fecha

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/ModuloAlgebraico.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Sugeridor/ModuloAlgebraico.java
@@ -14,16 +14,10 @@ public abstract class ModuloAlgebraico {
 	
 	public static Calendar redondearFecha(Calendar fecha) {
 		int hour = fecha.get(Calendar.HOUR_OF_DAY);
-		int day = fecha.get(Calendar.DAY_OF_MONTH);
-		if (hour == 24) {
-			fecha.set(Calendar.HOUR_OF_DAY, 0);
-			fecha.set(Calendar.DAY_OF_MONTH, day + 1);
-		} else if (hour == 23 || hour == 22) {
-			fecha.set(Calendar.HOUR_OF_DAY, 21);
-		} else if (hour % 3 == 1) {
-			fecha.set(Calendar.HOUR_OF_DAY, hour + 2);
+		if (hour % 3 == 1) {
+			fecha.add(Calendar.HOUR_OF_DAY, 2);
 		} else if (hour % 3 == 2) {
-			fecha.set(Calendar.HOUR_OF_DAY, hour + 1);
+			fecha.add(Calendar.HOUR_OF_DAY, 1);
 		}
 		return fecha;
 	}
