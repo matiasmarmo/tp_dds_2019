d2980ba8ad386594689b2afeceece1f4e22268ae
nicomelnyk <nicolasmelnyk@hotmail.com>
2019-08-05 13:54:17 -0300

    Se agrega test para el Ui

---
+++ QueMePongo/src/test/java/dds/utn/ju_ma/group7/QueMePongo/UiTest.java
@@ -0,0 +1,30 @@
+package dds.utn.ju_ma.group7.QueMePongo;
+
+import java.text.SimpleDateFormat;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import dds.utn.ju_ma.group7.QueMePongo.Ui.*;
+
+public class UiTest extends Fixture {
+	@Before
+	public void init() {
+	}
+
+	@Test
+	public void entreHaceTresDiasYManianaHayOchoEventos() {
+		SimpleDateFormat fecha1 = new SimpleDateFormat("yyyy-MM-dd");
+		String minus = fecha1.format(hace3DiasCalendar.getTime()); 
+		SimpleDateFormat fecha2 = new SimpleDateFormat("yyyy-MM-dd");
+		String plus = fecha2.format(manianaCalendar.getTime()); 
+		
+		ViewModel viewModel = new ViewModel();
+		viewModel.setFechaInicio(minus);
+		viewModel.setFechaFin(plus);
+		viewModel.cargarEventos();
+		
+		Assert.assertEquals(8, viewModel.getA().size());
+	}
+}
