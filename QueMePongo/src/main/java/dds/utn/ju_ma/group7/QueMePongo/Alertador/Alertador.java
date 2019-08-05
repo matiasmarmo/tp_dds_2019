754070c6e27879074c7230ed518f0cf4c78991b0
alejorodrig <alejo.meson@gmail.com>
2019-08-05 15:50:16 -0300

    Se implementa Job de Alertas

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Alertador/Alertador.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Alertador/Alertador.java
@@ -1,17 +1,30 @@
 package dds.utn.ju_ma.group7.QueMePongo.Alertador;
 
 import java.util.Calendar;
+import java.util.List;
 
+import dds.utn.ju_ma.group7.QueMePongo.Sugeridor.EstadoDelClima;
 import dds.utn.ju_ma.group7.QueMePongo.Sugeridor.ProveedorClima;
 
 public class Alertador {
 	
-	public void comprobarAlertas(Calendar fecha, ProveedorClima proveedor) {
-		// List<enum> = proveedor.getEstadoClima(fecha).obtenerAlertas()
+	private ProveedorClima proveedorClima;
+	
+	public Alertador(ProveedorClima proveedorClima) {
+		this.proveedorClima = proveedorClima;
+	}
+	
+	public void informarAlertas(Calendar fecha) {
+		this.obtenerAlertasParaLaFecha(fecha)
+			.forEach(alerta -> RepositorioUsuarios.informarUsuariosDe(fecha, alerta));
 	}
 	
 	public void informarDe(Calendar fecha, TipoAlerta alerta) {
 		RepositorioUsuarios.informarUsuariosDe(fecha, alerta);
+	}
+	
+	public List<TipoAlerta> obtenerAlertasParaLaFecha(Calendar fecha) {
+		return new EstadoDelClima(this.proveedorClima, fecha).getAlertas();
 	}
 
 }
---
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Alertador/JobAlertas.java
@@ -0,0 +1,34 @@
+package dds.utn.ju_ma.group7.QueMePongo.Alertador;
+
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import dds.utn.ju_ma.group7.QueMePongo.Sugeridor.ProveedorClima;
+
+public class JobAlertas extends TimerTask {
+	
+	public static Timer instanciarJobAlertas(ProveedorClima proveedorClima, long periodo) {
+		JobAlertas job = new JobAlertas(proveedorClima);
+		Timer timer = new Timer(false);
+		timer.scheduleAtFixedRate(job, 0, periodo);
+		return timer;
+	}
+	
+	private ProveedorClima proveedorClima;
+	
+	private JobAlertas(ProveedorClima proveedorClima) {
+		this.proveedorClima = proveedorClima;
+	}
+	
+	@Override
+	public void run() {
+		for(int i = 0; i < 3; i++) {
+			Calendar fecha = Calendar.getInstance();
+			fecha.add(Calendar.DATE, i);
+			Alertador alertador = new Alertador(this.proveedorClima);
+			alertador.informarAlertas(fecha);
+		}
+	}
+
+}
