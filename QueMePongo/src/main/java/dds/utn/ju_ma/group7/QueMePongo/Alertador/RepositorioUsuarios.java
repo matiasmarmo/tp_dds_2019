d7410a7d39d71c06bf53fd87094c5d5b08066a92
alejorodrig <alejo.meson@gmail.com>
2019-09-05 17:57:01 -0300

    Ahora el RepositorioUsuarios utiliza la BD
    Se deben correguir los test

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Alertador/RepositorioUsuarios.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Alertador/RepositorioUsuarios.java
@@ -1,42 +1,52 @@
 package dds.utn.ju_ma.group7.QueMePongo.Alertador;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
 import dds.utn.ju_ma.group7.QueMePongo.Usuario.InteresEnNotificaciones;
 import dds.utn.ju_ma.group7.QueMePongo.Usuario.Usuario;
 import dds.utn.ju_ma.group7.QueMePongo.Usuario.UsuarioGratis;
 import dds.utn.ju_ma.group7.QueMePongo.Usuario.UsuarioPremium;
 
-public class RepositorioUsuarios {
-	private List<Usuario> usuarios = new ArrayList<>();
-	
+public class RepositorioUsuarios implements TransactionalOps, WithGlobalEntityManager {
+
 	private static RepositorioUsuarios instance;
-	
-	private RepositorioUsuarios() {}
-	
+	private EntityManager entityManager = this.entityManager();
+
+	private RepositorioUsuarios() {
+	}
+
 	public static RepositorioUsuarios getInstance() {
-		if(instance == null) {
+		if (instance == null) {
 			instance = new RepositorioUsuarios();
 		}
 		return instance;
 	}
-	
+
 	public Usuario instanciarUsuarioGratis(List<InteresEnNotificaciones> notificadores) {
 		UsuarioGratis usuarioGratis = new UsuarioGratis(notificadores);
-		this.usuarios.add(usuarioGratis);
+		withTransaction(() -> {
+			this.entityManager.persist(usuarioGratis);
+		});
 		return usuarioGratis;
 	}
-	
+
 	public Usuario instanciarUsuarioPremium(List<InteresEnNotificaciones> notificadores) {
 		UsuarioPremium usuarioPremium = new UsuarioPremium(notificadores);
-		this.usuarios.add(usuarioPremium);
+		withTransaction(() -> {
+			this.entityManager.persist(usuarioPremium);
+		});
 		return usuarioPremium;
 	}
-	
+
 	public void informarUsuariosDe(Calendar fecha, TipoAlerta alerta) {
-		this.usuarios.forEach(usuario -> usuario.notificarAlerta(fecha, alerta));
+		this.entityManager.createQuery("from Usuario", Usuario.class).getResultList()
+				.forEach(usuario -> usuario.notificarAlerta(fecha, alerta));
 	}
 
 }
