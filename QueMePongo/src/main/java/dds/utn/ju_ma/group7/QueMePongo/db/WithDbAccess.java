8e2252c307d7a09ac56f9a1aa22179c5694cc85c
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-09-06 02:25:20 -0300

    Actualizar WithDbAccess

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/db/WithDbAccess.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/db/WithDbAccess.java
@@ -2,20 +2,15 @@ package dds.utn.ju_ma.group7.QueMePongo.db;
 
 import java.util.Collection;
 
+import org.uqbarproject.jpa.java8.extras.EntityManagerOps;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.transaction.TransactionalOps;
 
-public interface WithDbAccess extends WithGlobalEntityManager, TransactionalOps {
-	
-	default void persist(Object objectToPersist) {
-		this.withTransaction(() -> {
-			this.entityManager().persist(objectToPersist);
-		});
-	}
+public interface WithDbAccess extends WithGlobalEntityManager, TransactionalOps, EntityManagerOps {
 	
 	default <T> void persist(Collection<T> collectionToPersist) {
 		this.withTransaction(() -> {
-			collectionToPersist.forEach((element) -> this.entityManager().persist(element));
+			collectionToPersist.forEach((element) -> this.persist(element));
 		});
 	}
 
