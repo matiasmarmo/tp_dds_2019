873eb108f4c4175550eae3abd800c9771cff602b
alejorodrig <alejo.meson@gmail.com>
2019-10-25 22:30:53 -0300

    Los colores ahora se muestran correctamente
    closes #50

--- QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Prenda/Color.java
+++ QueMePongo/src/main/java/dds/utn/ju_ma/group7/QueMePongo/Prenda/Color.java
@@ -25,14 +25,12 @@ public class Color {
 		this.rojo = rojo;
 		this.verde = verde;
 		this.azul = azul;
-		this.rgb = "#" + Integer.toHexString(rojo) + Integer.toHexString(verde) + Integer.toHexString(azul);
+		this.rgb = "#" + String.format("%02X", rojo) + String.format("%02X", verde) + String.format("%02X", azul);
 	}
-	
+
 	public static Color hexToRgb(String colorStr) {
-	    return new Color(
-	            Integer.valueOf( colorStr.substring( 1, 3 ), 16 ),
-	            Integer.valueOf( colorStr.substring( 3, 5 ), 16 ),
-	            Integer.valueOf( colorStr.substring( 5, 7 ), 16 ) );
+		return new Color(Integer.valueOf(colorStr.substring(1, 3), 16), Integer.valueOf(colorStr.substring(3, 5), 16),
+				Integer.valueOf(colorStr.substring(5, 7), 16));
 	}
 
 	public int getRojo() {
@@ -46,7 +44,7 @@ public class Color {
 	public int getAzul() {
 		return azul;
 	}
-	
+
 	public String getRgb() {
 		return this.rgb;
 	}
